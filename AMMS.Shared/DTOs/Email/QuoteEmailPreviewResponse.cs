f67449c2592a8a0b279a32f11f1ec7d8197957c4
truongpm234 <phamminhtruong2032004@gmail.com>
2026-03-26 12:40:07 +0700

    [UPDATE][EmailPreview]: add contract

--- AMMS.Shared/DTOs/Email/QuoteEmailPreviewResponse.cs
+++ AMMS.Shared/DTOs/Email/QuoteEmailPreviewResponse.cs
@@ -42,8 +42,8 @@ namespace AMMS.Shared.DTOs.Email
         public string? order_detail_url { get; set; }
         public bool is_customer_copy { get; set; }
         public string? email_html { get; set; }
-        public string? contract_file_path { get; set; }
-        public DateTime? contract_uploaded_at { get; set; }
+        public string? consultant_contract_path { get; set; }
+        public string? customer_signed_contract_path { get; set; }
     }
 
 }
